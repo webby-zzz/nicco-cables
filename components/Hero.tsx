6bbb70140f64dd984c9372a0d175032c3492c8fb
Jared Official <jared@example.com>
2026-07-04 13:15:03 +0530

    style(hero): update aspect ratio to 3:1 (1920x640px)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Hero: React.FC = () => {
   return (
-    <section className="relative w-full aspect-[2/1] lg:aspect-auto lg:h-[90vh] bg-black overflow-hidden lg:mt-20">
+    <section className="relative w-full aspect-[3/1] bg-black overflow-hidden lg:mt-20">
       <video
         src="/brand%20identity/full%20width%20NiccoCables_WebBanner.mp4"
         autoPlay
